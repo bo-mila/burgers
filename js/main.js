eda15f4129c54edd37b98b562c5ac2dc0904dee4
Mila Bozhevskaya <mi.bozhev@gmail.com>
2021-02-03 19:58:16 +0200

    add hamburger menu, animation hamburger-link, acco team

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const hamburgerMenuInput = document.querySelector('.hamburger-menu__input');
+const hamburger = document.querySelector('.hamburger');
+const headerNav = document.querySelector('.header__nav');
+
+const team = document.querySelector('.team');
+const teamItem = document.querySelectorAll('.team__item');
+const teamItemLength = teamItem.length;
+
+function noScroll() {
+  window.scrollTo(0, 0);
+}
+
+//////////// HAMBURGER ///////////////
+
+hamburgerMenuInput.addEventListener("click", function(e) {
+  e.preventDefault;
+  if (hamburgerMenuInput.checked == true) {
+    hamburger.style.display = "flex";
+    headerNav.style.display = "none";
+    window.addEventListener('scroll', noScroll); 
+  } else {
+    hamburger.style.display = "none";
+    headerNav.style.display = "flex";
+    window.removeEventListener('scroll', noScroll);
+  }
+});
+
+//////////// ACCO VERTICAL -> TEAM ///////////
+
+team.addEventListener('click', function(e) {
+  e.preventDefault();
+  for (let i=0; i < teamItemLength; i++) {
+    teamItem[i].classList.remove('team__item--active');
+  }
+});
+for (let i=0; i<teamItemLength; i++) {
+  teamItem[i].addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (teamItem[i].classList.contains('team__item--active')) {
+      teamItem[i].classList.remove('team__item--active');
+    } else {
+      for (let i=0; i<teamItemLength; i++) {
+        teamItem[i].classList.remove('team__item--active');
+      }
+      teamItem[i].classList.add('team__item--active');
+    }
+  });
+}
