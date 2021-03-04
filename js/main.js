c11460e9a58938696ece97cb9622917bbfa1065c
Mila Bozhevskaya <mi.bozhev@gmail.com>
2021-03-04 10:40:01 +0200

    form works with answer-overlay, validation field phone - only numbers+dash+backspace

--- js/main.js
+++ js/main.js
@@ -113,8 +113,6 @@ burger.addEventListener('click', function(event) {
   event.preventDefault();
 });
 
-
-
 function createSlider() {
   createSlideView();
   createSlideLeft();
@@ -157,7 +155,6 @@ function createSlideRight() {
   slider.appendChild(li);
 }
 
-
 function left() {
   sliderArrowLeft.onclick = null;
   let sliderL = document.querySelectorAll('.slider__item');
@@ -172,21 +169,18 @@ function left() {
   if (slideNumberView == sliderLength) {
     slideNumberView = 0;
   }
-  console.log(slideNumberView);
   setTimeout(function () {
     sliderL[0].remove();
     createSlideRight();
     sliderArrowLeft.onclick = left;
   }, 600);
 }
-
 function right() {
   sliderArrowRight.onclick = null;
   let sliderR = document.querySelectorAll('.slider__item');
   let offsetR = (slideView+1);
   
   for (let i = (slideView+1); i>-1; i--) {
-    console.log(i, offsetR);
     sliderR[i].style.left = offsetR*100/slideView + '%';
     offsetR--;
   }
@@ -203,13 +197,138 @@ function right() {
 
 
 
+/////////////////// FORM  ///////////////////////
+
+const form = document.querySelector('#form');
+const buttonsSub = document.querySelector('.buttons__sub');
+const buttonsRes = document.querySelector('.buttons__res');
+
+buttonsRes.addEventListener('click', function(event) {
+  event.preventDefault();
+  form.reset();  
+});
+
+buttonsSub.addEventListener('click', function(event) {
+  event.preventDefault();
+ 
+    const formData = new FormData();
+    formData.append('name',  form.elements.name.value);
+    formData.append('phone',  form.elements.phone.value);
+    formData.append('comment',  form.elements.comment.value);
+    formData.append('to',  'mi@gmail.com');
+
+  if (validForm(form)) {
+    
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('POST','https://webdev-api.loftschool.com/sendmail');
+    xhr.send(formData);
+    xhr.addEventListener('load', () => {
+      if ((xhr.response.status)|(xhr.response.status==0)) {
+        overlayMessage(xhr.response.message);
+        form.reset();
+      }
+    });
+  }
+
+});
+
+function validForm (form) {
+  let valid = true;
+
+  if (!validField(form.elements.name)) {
+    valid = false;
+  }
+
+  if (!validField(form.elements.phone)) {
+    valid = false;
+  }
+  if (!validField(form.elements.comment)) {
+    valid = false;
+  }
+  return valid;
+}
+
+function validField(field) {
+  field.nextElementSibling.textContent = field.validationMessage;
+  return field.checkValidity();
+}
 
 
 
+//////////////////    --------OVERLAY------------
+const template = document.querySelector('#overlayTemp').innerHTML;
+const overlay = createOverlay(template);
 
+function overlayMessage(message) {
+  overlay.open();
+  overlay.setContent(message);
+  document.body.style.overflow = "hidden";
+  
+  
+}
+function createOverlay(template) {
+  
+  const fragment = document.createElement('div');
 
+  
+  fragment.innerHTML = template;
+  const overlayElement = fragment.querySelector(".overlay");
+  const contentElement = fragment.querySelector(".overlay__content");
+  const closeElement = fragment.querySelector(".overlay__close");
 
+  overlayElement.addEventListener("click", e => {
+    if (e.target === overlayElement) {
+      closeElement.click();
+      document.body.style.overflow = "initial";
+    }
+  });
 
+  closeElement.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.body.removeChild(overlayElement);
+    document.body.style.overflow = "initial";
+  });
 
+  return {
+    open() {
+       
+      document.body.appendChild(overlayElement);
+     
+    },
+    close() {
+      
+      closeElement.click();
+    },
+    setContent(message) {
+      contentElement.innerHTML = message;
+    },
+  };
+}
 
-
+//////////////валидация поля ввода телефона
+let formPhoneValid = document.querySelector('#phone');
+let valueReturn = '';
+formPhoneValid.addEventListener('keydown', e => {
+  let keyName = e.key;
+  e.preventDefault();
+  if (((keyName >= '0' && keyName <= "9") || (keyName ==='-')) && (valueReturn.length < 16)) {
+    if (formPhoneValid.selectionStart != formPhoneValid.selectionEnd) {
+      formPhoneValid.setRangeText('');
+      valueReturn = formPhoneValid.value + keyName;
+    } else {
+      valueReturn = valueReturn + keyName;
+    }
+    return formPhoneValid.value = valueReturn;
+  } else {
+    if (keyName === "Backspace") {
+      if (formPhoneValid.selectionStart != formPhoneValid.selectionEnd) {
+        formPhoneValid.setRangeText('');
+        valueReturn = formPhoneValid.value;
+      } else {
+        valueReturn = valueReturn.substring(0,valueReturn.length - 1);
+      }
+      return formPhoneValid.value = valueReturn;
+    }
+  }
+});
